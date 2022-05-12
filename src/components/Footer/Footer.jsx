18410c9c98888a2e4794b8b8b586cf04e7bddeee
hamadbd <hamaddwairi0@gmail.com>
2022-05-12 13:26:34 +0300

    ReactApp completed

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+         <div>
+            <a href="www.github.com/hamadbd">Github</a>
+            <p/>
+            <a href="www.linkedin.com/in/hamad-dwairi-2608aa202/">LinkedIn</a>
+            <p> copy rights reserved HamadBD Productions 2022</p>
+        </div>
+    )
+}
+
+export default Footer;
